baac2b63943f5be1a0d24f1015aa580de63286eb
AbbyAdj <abbyadjei9@gmail.com>
2025-03-02 17:50:36 +0000

    Add Procfile

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from __future__ import annotations
 
 from hashlib import md5
 from typing import List
-
+import os
+from dotenv import load_dotenv
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_migrate import Migrate
@@ -38,7 +39,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -52,7 +53,7 @@ login_manager.init_app(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI",'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 migrate = Migrate(app, db, command="migrate")
